9f9b1efce18113529e53b6e5921dc7e31367fff8
Pieter <piethboterhoek@gmail.com>
2025-10-17 11:04:37 +0200

    mail upd

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => {
         <a href="https://github.com/yoshiappel" target="_blank">
           <img src={githublogo} className="logo" alt="Github logo" />
         </a>
-        <a href="mailto:piethboterhoek@gmail.com" target="_blank">
+        <a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#112;&#105;&#101;&#116;&#104;&#098;&#111;&#116;&#101;&#114;&#104;&#111;&#101;&#107;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" target="_blank">
           <img src={maillogo} className="logo" alt="Mail logo" />
         </a>
         <a href="https://linkedin.com/in/piethendrikboterhoek" target="_blank">
